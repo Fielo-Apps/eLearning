16f2def6c71058b9707ffa5144869444c4e0523d
Tiago Leal <tibeal@gmail.com>
2020-07-09 17:11:37 -0300

    new parameter

--- force-app/main/default/aura/Courses/CoursesHelper.js
+++ force-app/main/default/aura/Courses/CoursesHelper.js
@@ -154,7 +154,7 @@
                 modulesFieldset += ',FieloELR__AttemptsAllowed__c';
             }
             var dynamicFilterString = component.get('v.dynamicFilterString');
-            var sortByClause = component.get('v.sortByClause');
+            var sortByClause = component.get('v.sortByClause') || component.get('v.defaultSortBy');
             if(member){            
                 var action;
                 var activeViewName = component.get('v.activeViewName');
