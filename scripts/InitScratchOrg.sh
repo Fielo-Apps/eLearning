f9255dea7b3b0dc93053422cf674a770995d520e
Tiago Leal <tibeal@gmail.com>
2020-08-07 12:35:09 -0300

    fixed init scratch org

--- scripts/InitScratchOrg.sh
+++ scripts/InitScratchOrg.sh
@@ -1,16 +1,19 @@
 # !/bin/bash
 
 # Open on Deployment Page
-sfdx force:org:open -p /lightning/setup/DeployStatus/home
+sfdx force:org:open -p /lightning/setup/DeployStatus/home &&
 
 # Install FieloPLT
-sfdx force:package:install --package 04t2J000000atTQ -w 60
+sfdx force:package:install --package 04t2J000000Exf2 -w 60 &&
+
+# Push Source
+sfdx force:source:push -w 60 &&
+
+# Assign the permission set to the user
+sfdx force:user:permset:assign --permsetname FieloELRAdmin &&
+
+# Deploy Unpackaged
+sfdx force:source:deploy -p unpackaged/ &&
 
 # Push Source
 sfdx force:source:push -w 60
-
-# Assign the permission set to the user
-sfdx force:user:permset:assign --permsetname FieloELRAdmin
-
-# Deploy Unpackaged
-sfdx force:source:deploy -p unpackaged/
