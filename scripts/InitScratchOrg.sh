85a8dd37223095098275e9ab200f1720b085193d
Tiago Leal <tibeal@gmail.com>
2020-08-07 13:18:03 -0300

    enhanced initscratchorg

--- scripts/InitScratchOrg.sh
+++ scripts/InitScratchOrg.sh
@@ -6,14 +6,14 @@ sfdx force:org:open -p /lightning/setup/DeployStatus/home &&
 # Install FieloPLT
 sfdx force:package:install --package 04t2J000000Exf2 -w 60 &&
 
+# ADD Unpackaged into folder
+ln -s ../../unpackaged ./force-app/main/unpackaged &&
+
 # Push Source
 sfdx force:source:push -w 60 &&
 
 # Assign the permission set to the user
 sfdx force:user:permset:assign --permsetname FieloELRAdmin &&
 
-# Deploy Unpackaged
-sfdx force:source:deploy -p unpackaged/ &&
-
-# Push Source
-sfdx force:source:push -w 60
+# REMOVE Unpackaged from folder
+rm ./force-app/main/unpackaged
