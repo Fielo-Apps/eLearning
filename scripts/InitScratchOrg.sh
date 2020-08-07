33e1ea5c3fd0a4751274e108a9b5717738dbd441
Tiago Leal <tibeal@gmail.com>
2020-08-06 22:22:23 -0300

    added new pakg

--- scripts/InitScratchOrg.sh
+++ scripts/InitScratchOrg.sh
@@ -4,7 +4,7 @@
 sfdx force:org:open -p /lightning/setup/DeployStatus/home
 
 # Install FieloPLT
-sfdx force:package:install --package 04t2J000000atTQ -w 60
+sfdx force:package:install --package 04t2J000000IzIm -w 60
 
 # Push Source
 sfdx force:source:push -w 60
