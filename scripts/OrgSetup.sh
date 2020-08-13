eb925c73fb783cbfb476055744529e4428c0c6ca
Tiago Leal <tibeal@gmail.com>
2020-08-13 08:55:31 -0300

    fixed stuff

--- scripts/OrgSetup.sh
+++ scripts/OrgSetup.sh
@@ -5,3 +5,6 @@ sfdx force:apex:execute -f scripts/ActivateLibraries.apex
 
 # Create Program and Members
 sfdx force:apex:execute -f scripts/CreateMembers.apex
+
+# Create Sample Course
+sfdx force:apex:execute -f scripts/createSmokeTestCourse.apex
