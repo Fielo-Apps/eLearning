9ff179f196429a94a3039396e0f05e844de52a30
Tiago Leal <tibeal@gmail.com>
2020-11-17 18:36:09 -0300

    fixed scripts folder structure

--- scripts/OrgSetup.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-# Activate Libraries
-sfdx force:apex:execute -f scripts/ActivateLibraries.apex
-
-# Create Program and Members
-sfdx force:apex:execute -f scripts/CreateMembers.apex
--- scripts/shell/InitScratchOrg.sh
+++ scripts/shell/InitScratchOrg.sh
@@ -4,19 +4,13 @@
 sfdx force:org:open -p /lightning/setup/DeployStatus/home &&
 
 # Install FieloPLT
-sfdx force:package:install --package 04t2J000000ExfH -w 60 &&
-
-# ADD Unpackaged into folder
-# ln -s ../../unpackaged ./force-app/main/unpackaged &&
+sfdx force:package:install --package 04t2J000000AlpD -w 60 &&
 
 # Push Source
 sfdx force:source:push -w 60 &&
 
 # Assign the permission set to the user
 sfdx force:user:permset:assign --permsetname FieloELRAdmin &&
-
-# REMOVE Unpackaged from folder
-# rm ./force-app/main/unpackaged
 
 # DEPLOY Unpackaged folder
 sfdx force:source:deploy -p ./unpackaged/
