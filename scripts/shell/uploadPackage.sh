e40da1da643cb6821e42fd680ae16062bfa6f99b
Tiago Leal <tibeal@gmail.com>
2021-04-29 11:17:13 -0300

    helper shell script

--- scripts/shell/uploadPackage.sh
+++ scripts/shell/uploadPackage.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-sfdx force:package1:version:create -u patch-elr-1.76 --packageid 0336A000000PEHg --name "Fielo ELR 1.76.1"  --version "1.76" --managedreleased
+sfdx force:package1:version:create -u patch-elr-1.76 --packageid 0336A000000PEHg --name "Fielo ELR 1.76.2"  --version "1.76" --managedreleased
